fba17dcef4ca8be18549c47d2b6cca5873983f4e
Esmi415 <esveralinares@s.sfusd.edu>
2025-12-02 11:30:19 -0800

    mid lesson 4

--- M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,7 +1,7 @@
 extends Sprite2D
 
 var velocity := Vector2(480, -480)
-var max_speed := 600.0
+var max_speed := normal_speed 
 
 
 func _process(delta: float)-> void:
@@ -12,8 +12,16 @@ func _process(delta: float)-> void:
 	if direction.length() > 1.0:
 		direction = direction.normalized()
 		
+	var boost_speed := 1500.0
+	if Input.is_action_just_pressed("boost"):
+	max_speed = boost_speed
+	get_node("Timer").start()
 	velocity = direction * max_speed
 	position += velocity * delta
 	
 	if direction.length() > 0.0:
 		rotation = velocity.angle()
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
